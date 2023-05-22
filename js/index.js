57337a05549430cfe9d49a769eb0f0c1e97d59f4
tmphat1312 <tmpsummer@gmail.com>
2023-05-22 19:10:34 +0700

    reformat files, change file structure for performace best practices

---
+++ js/index.js
@@ -0,0 +1,34 @@
+const navTogglerBox = document.querySelector('.navigation-toggler-box')
+const navToggler = document.querySelector('.navigation-toggler')
+const nav = document.querySelector('.navigation')
+const header = document.querySelector('header')
+
+navTogglerBox.addEventListener('click', ({ target }) => {
+  navToggler.classList.toggle('active')
+  nav.classList.toggle('active')
+})
+
+document.addEventListener('click', ({ target }) => {
+  if (target != navTogglerBox && target != navToggler) {
+    navToggler.classList.remove('active')
+    nav.classList.remove('active')
+  }
+})
+
+window.addEventListener('scroll', handleStickyHeader)
+window.addEventListener('load', handleStickyHeader)
+
+function handleStickyHeader({ target }) {
+  const intro = document.querySelector('.intro')
+  const introHeight = intro.offsetHeight
+  const headerHeight = header.offsetHeight
+  const introTop = document.querySelector('.intro').getBoundingClientRect().top
+
+  if (introTop < headerHeight - introHeight) {
+    header.classList.add('fixed')
+    intro.style.marginTop = `${headerHeight}px`
+  } else {
+    header.classList.remove('fixed')
+    intro.style.marginTop = 'unset'
+  }
+}
